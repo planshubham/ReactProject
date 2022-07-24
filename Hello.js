a0953977d5aa25a92e1e44b2f2c85a635554664a
Shubham Patil <patil.shubham1@indiamart.com>
2022-07-24 21:15:23 +0530

    newchanges

--- Hello.js
+++ Hello.js
@@ -1,2 +1,51 @@
-console.log("Hii");
-debugger;
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Hello extends Component {
+    constructor(props) {
+        super(props);
+
+    }
+
+    componentWillMount() {
+
+    }
+
+    componentDidMount() {
+
+    }
+
+    componentWillReceiveProps(nextProps) {
+
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+
+    }
+
+    componentWillUpdate(nextProps, nextState) {
+
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+
+    }
+
+    componentWillUnmount() {
+
+    }
+
+    render() {
+        return (
+            <div>
+  <h1> i am in main</h1>
+            </div>
+        );
+    }
+}
+
+Hello.propTypes = {
+
+};
+
+export default Hello;
